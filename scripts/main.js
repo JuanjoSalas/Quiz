c4356eed3c44da72eb28e12c58cb9f50c8f2b1b8
juan salas <juanjo.salas.dev@gmail.com>
2024-04-12 13:01:23 +0200

    Estructura mejorada

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,28 @@
 const homeNav = document.getElementById("homeNav");
-const questionNav = document.getElementById("questionNav");
+const quizNav = document.getElementById("quizNav");
 const resultsNav = document.getElementById("resultsNav");
 const homeDiv = document.getElementById("home");
-const questionDiv = document.getElementById("question");
+const quizDiv = document.getElementById("quiz");
 const resultsDiv = document.getElementById("results");
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
 
 const hideViews =()=>{
     homeDiv.classList.add("hide")
-    questionDiv.classList.add("hide")
+    quizDiv.classList.add("hide")
     resultsDiv.classList.add("hide")
 }
 const showHome =()=>{
     hideViews()
     homeDiv.classList.remove("hide")
 }
-const showQuestion =()=>{
+const showQuiz =()=>{
     hideViews()
-    questionDiv.classList.remove("hide")
+    quizDiv.classList.remove("hide")
 }
 const showResults =()=>{
     hideViews()
@@ -24,5 +30,5 @@ const showResults =()=>{
 }
 
 homeNav.addEventListener("click",showHome)
-questionNav.addEventListener("click",showQuestion)
+quizNav.addEventListener("click",showQuiz)
 resultsNav.addEventListener("click",showResults)
