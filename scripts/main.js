c62922847026db606b3f50c7465eb78af36aeca9
juan salas <juanjo.salas.dev@gmail.com>
2024-04-15 17:33:25 +0200

    tenQuestions

--- scripts/main.js
+++ scripts/main.js
@@ -15,120 +15,139 @@ const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
 // variables
 let questions = [];
-let correctAnswer = ""
+let correctAnswer = "";
+let incorrectAnswer = [];
 let currentQuestionIndex;
-
+let arrbtn = [];
 // Fuctions
 // Body
-const hideViews =()=>{
-    homeDiv.classList.add("hide")
-    quizDiv.classList.add("hide")
-    resultsDiv.classList.add("hide")
-}
-const showHome =()=>{
-    hideViews()
-    homeDiv.classList.remove("hide")
-}
-const showQuiz =()=>{
-    hideViews()
-    quizDiv.classList.remove("hide")
-}
-const showResults =()=>{
-    hideViews()
-    resultsDiv.classList.remove("hide")
-}
+const hideViews = () => {
+  homeDiv.classList.add("hide");
+  quizDiv.classList.add("hide");
+  resultsDiv.classList.add("hide");
+};
+const showHome = () => {
+  hideViews();
+  homeDiv.classList.remove("hide");
+};
+const showQuiz = () => {
+  hideViews();
+  quizDiv.classList.remove("hide");
+};
+const showResults = () => {
+  hideViews();
+  resultsDiv.classList.remove("hide");
+};
 
 //axios
-//axios.get("https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple")
-//.then((res) => questions =  res.data.results)
-//.catch((err) => console.error(err));
-
-//axios.get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
-//.then((res) => console.log(res.data))
-//.then((res) => questions = (res.data))
-//.catch((err) => console.error(err));
-
-const loadQuestion = async ()=>{
-    try {
-        const result = await axios.get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
-        questions = (result.data);
-        //console.log(result.data[0]);
-        console.log(answer);
-    } catch (error) {
-        console.error(error);
-    }
-}
-loadQuestion()
-
-const startGame =()=>{
-    startButton.classList.add("hide")
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide")
+const loadQuestion = async () => {
+  try {
+    const result = await axios.get(
+      "https://thesimpsonsquoteapi.glitch.me/quotes?count=10"
+    );
+    questions = result.data;
+    console.log(questions);
     setNextQuestion();
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-/*let incorrectAnswer = questions
-console.log(incorrectAnswer);
-const incorrectAnswer =()=>{
+
+/*const loadAnswer = async ()=>{
+    const result = await axios.get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
+    questions = (result.data);
+} catch (error) {
+    console.error(error);
 }*/
 
-const showQuestion =(question)=>{
-questionElement.innerHTML = 
-`<img src="${question.image}" alt=""></img>`;
+/*const showAnswer =()=>{
+    if (questions.character !== correctAnswer) {
+        return incorrectAnswer.push(questions);
+    }
+}
+console.log(incorrectAnswer);*/
 
-correctAnswer = question.character;
-const answers = questions.map(question=>question.character);
-//let optionsList = incorrectAnswer;
-//optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
-console.log(answers);
-    answers.forEach((answer) => {
-    const button = document.createElement("button")
+const startGame = () => {
+  startButton.classList.add("hide");
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  loadQuestion();
+  setNextQuestion();
+};
+
+const showQuestion = (question) => {
+  currentQuestionIndex = currentQuestionIndex; //PREG A SOFI XQ NO FUCNIONA
+  console.log(question);
+  console.log(question[currentQuestionIndex].image);
+
+  const answers = questions.map((question) => question.character);
+  //const answers = incorrectAnswer;
+  let filteredanswers = answers.filter(
+    (item, index) => answers.indexOf(item) == index
+  );
+  filteredanswers.forEach((element) => {arrbtn.push(element);
+  });
+  let arrbtnFiltered = arrbtn.filter(    (item, index) => arrbtn.indexOf(item) == index
+  );    
+  console.log(arrbtnFiltered);
+
+    if (arrbtnFiltered.length < 10) {
+      loadQuestion()
+
+    }
+  questionElement.innerHTML = `<img src="${question[currentQuestionIndex].image}" alt=""></img>`;
+  correctAnswer = question[currentQuestionIndex].character;
+  arrbtnFiltered.sort()
+  arrbtnFiltered.forEach((answer) => {
+    const button = document.createElement("button");
     button.innerText = answer;
-
     if (answer == correctAnswer) {
-        button.dataset.correct = true;
+      button.dataset.correct = true;
     }
     button.addEventListener("click", selectAnswer);
     answerButtonsElement.appendChild(button);
-});
-}
+  });
+};
 
-const setNextQuestion =()=>{
-    resetState();
-    showQuestion(questions[currentQuestionIndex]);
-}
+const setNextQuestion = () => {
+  resetState();
+  showQuestion(questions);
+};
 
-const setStatusClass =(option)=>{
-    if (option.dataset.correct) {
-        option.classList.add("correct");
-    } else {
-        option.classList.add("wrong");
-    }
-}
+const setStatusClass = (option) => {
+  if (option.dataset.correct) {
+    option.classList.add("correct");
+  } else {
+    option.classList.add("wrong");
+  }
+};
 
-const selectAnswer =()=>{
-    Array.from(answerButtonsElement.children).forEach((button) =>{
-        setStatusClass(button)
-    })
-    if (questions.length > currentQuestionIndex +1) {
-        nextButton.classList.remove("hide");
-    } else {
-        startButton.innerText = "restart"
-        startButton.classList.remove("hide");
-    }
-}
+const selectAnswer = () => {
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button);
+  });
+  //if (questions.length > currentQuestionIndex +1) {
+  if (10 > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    showResults();
+    startButton.innerText = "restart";
+    startButton.classList.remove("hide");
+  }
+};
 
-const resetState =()=>{
-    nextButton.classList.add("hide");
-    answerButtonsElement.innerHTML=""
-} 
+const resetState = () => {
+  nextButton.classList.add("hide");
+  answerButtonsElement.innerHTML = "";
+};
 
 // Events
-homeNav.addEventListener("click",showHome)
-quizNav.addEventListener("click",showQuiz)
-resultsNav.addEventListener("click",showResults)
-startButton.addEventListener("click",startGame)
-nextButton.addEventListener("click", ()=>{
-    currentQuestionIndex++;
-    setNextQuestion();
-})
+homeNav.addEventListener("click", showHome);
+quizNav.addEventListener("click", showQuiz);
+resultsNav.addEventListener("click", showResults);
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
