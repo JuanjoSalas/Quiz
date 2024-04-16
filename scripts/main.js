60e8026138c14c1f70c61eb5abffb23c65d75db7
juan salas <juanjo.salas.dev@gmail.com>
2024-04-16 13:06:28 +0200

    Four Answers

--- scripts/main.js
+++ scripts/main.js
@@ -9,10 +9,18 @@ const quizDiv = document.getElementById("quiz");
 const resultsDiv = document.getElementById("results");
 // Question
 const startButton = document.getElementById("start-btn");
+const startHardButton = document.getElementById("start-btn-hard");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+// Result
+const ralphDiv = document.getElementById("ralph");
+const nelsonDiv = document.getElementById("nelson");
+const bartDiv = document.getElementById("bart");
+const milhouseDiv = document.getElementById("milhouse");
+const martinDiv = document.getElementById("martin");
+const lisaDiv = document.getElementById("lisa");
 // variables
 let questions = [];
 let correctAnswer = "";
@@ -26,6 +34,12 @@ const hideViews = () => {
   homeDiv.classList.add("hide");
   quizDiv.classList.add("hide");
   resultsDiv.classList.add("hide");
+  ralphDiv.classList.add("hide");
+  nelsonDiv.classList.add("hide");
+  bartDiv.classList.add("hide");
+  milhouseDiv.classList.add("hide");
+  martinDiv.classList.add("hide");
+  lisaDiv.classList.add("hide");
 };
 const showHome = () => {
   hideViews();
@@ -38,20 +52,19 @@ const showQuiz = () => {
 const showResults = () => {
   hideViews();
   resultsDiv.classList.remove("hide");
-  if (score < 2) {
-    console.log("Ralfh");
-  } if (score < 4) {
-    console.log("Nelson");
+  if (score  < 2) {
+    ralphDiv.classList.remove("hide");
+  } if (score  < 4) {
+    nelsonDiv.classList.remove("hide");
   } if (score < 6) {
-    console.log("Bart");
+    bartDiv.classList.remove("hide");
   } if (score < 8) {
-    console.log("Milhouse");
+    milhouseDiv.classList.remove("hide");
   } if (score < 10) {
-    console.log("Martin");
-  } if (score = 10) {
-    console.log("Lisa");
+    martinDiv.classList.remove("hide");
+  } else {
+    lisaDiv.classList.remove("hide");
   }
-  console.log(score);
 };
 
 //axios
@@ -69,27 +82,23 @@ const loadQuestion = async () => {
 };
 loadQuestion()
 
-/*const loadAnswer = async ()=>{
-    const result = await axios.get("https://thesimpsonsquoteapi.glitch.me/quotes?count=10")
-    questions = (result.data);
-} catch (error) {
-    console.error(error);
-}*/
-
-/*const showAnswer =()=>{
-    if (questions.character !== correctAnswer) {
-        return incorrectAnswer.push(questions);
-    }
-}
-console.log(incorrectAnswer);*/
-
 const startGame = () => {
   startButton.classList.add("hide");
+  startHardButton.classList.add("hide");
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   //loadQuestion();
   setNextQuestion();
 };
+
+/*const startGameHard = () => {
+  startButton.classList.add("hide");
+  startHardButton.classList.add("hide");
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  //loadQuestion();
+  setNextQuestionHard();
+};*/
 
 const showQuestion = (question) => {
   currentQuestionIndex = currentQuestionIndex; //PREG A SOFI XQ NO FUCNIONA
@@ -108,7 +117,62 @@ const showQuestion = (question) => {
     if (arrbtnFiltered.length < 10) {
       loadQuestion()
     }
+    /*if (arrbtnFiltered.length > 10) {
+      arrbtnFiltered.pop()
+      showQuestion(question)
+    }*/
   questionElement.innerHTML = `<img src="${question[currentQuestionIndex].image}" alt=""></img>`;
+  correctAnswer = question[currentQuestionIndex].character;
+  const arrSort = arrbtnFiltered.filter((answer,i) => {
+    if (answer == correctAnswer) {
+      return answer
+    }
+    if(i<4 && answer !== correctAnswer){
+      return answer
+    }
+  })
+  arrSort.sort()
+  arrSort.forEach((answer) => {
+    let button = document.createElement("button");
+button.innerText=answer
+   console.log(answer);
+    if (answer == correctAnswer) {
+      //score.push(1);
+      score++;
+      button.dataset.correct = true;
+    }
+    button.addEventListener("click", selectAnswer);
+
+    /*button.addEventListener("click",() =>{
+      score++;
+
+      selectAnswer
+  }*/ 
+   
+      answerButtonsElement.appendChild(button);  
+    
+  });
+};
+console.log(score);
+
+/*const showQuestionHard = (question) => {
+  currentQuestionIndex = currentQuestionIndex; //PREG A SOFI XQ NO FUCNIONA
+  console.log(question);
+  console.log(question[currentQuestionIndex].quote);
+  const answers = questions.map((question) => question.character);
+  let filteredanswers = answers.filter(
+    (item, index) => answers.indexOf(item) == index
+  );
+  filteredanswers.forEach((element) => {arrbtn.push(element);
+  });
+  let arrbtnFiltered = arrbtn.filter(    (item, index) => arrbtn.indexOf(item) == index
+  );    
+  console.log(arrbtnFiltered);
+
+    if (arrbtnFiltered.length < 10) {
+      loadQuestion()
+    }
+  questionElement.innerHTML = `<h5>${question[currentQuestionIndex].quote}</h5>`;
   correctAnswer = question[currentQuestionIndex].character;
   arrbtnFiltered.sort()
   arrbtnFiltered.forEach((answer) => {
@@ -123,11 +187,17 @@ const showQuestion = (question) => {
     answerButtonsElement.appendChild(button);
   });
 };
-console.log(score);
+console.log(score);*/
+
 const setNextQuestion = () => {
   resetState();
   showQuestion(questions);
 };
+
+/*const setNextQuestionHard = () => {
+  resetState();
+  showQuestionHard(questions);
+};*/
 
 const setStatusClass = (option) => {
   if (option.dataset.correct) {
@@ -161,6 +231,7 @@ homeNav.addEventListener("click", showHome);
 quizNav.addEventListener("click", showQuiz);
 resultsNav.addEventListener("click", showResults);
 startButton.addEventListener("click", startGame);
+//startHardButton.addEventListener("click", startGameHard);
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
